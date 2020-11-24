433f97fa1eae01e0bac6cebd57deb9f8f4ea9eee
ramram1048 <ramram1048@gmail.com>
2020-11-24 18:00:16 +0900

    feat: 메인 페이지 스크립트 작성 #10
    - 버튼, form에 id 부여, eventListener 연결
    - TODO: 에러 컨트롤

--- src/frontend/main/index.js
+++ src/frontend/main/index.js
@@ -1,4 +1,31 @@
 import './index.scss';
+import request from '../utils/requestHandler';
 
-// document.getElementById('root').innerText = 'Hello duxit!';
-console.log('Hello duxit!');
+const redirectToGameRoom = (code) => {
+  window.location.href = `/game?room=${code}`;
+};
+
+const requestMakeRoom = async (e) => {
+  e.preventDefault();
+  const config = { method: 'POST', uri: '/rooms' };
+  const { ok, code } = await request(config);
+  if (ok) redirectToGameRoom(code);
+};
+
+const requestEnterRoom = async (e) => {
+  e.preventDefault();
+  const code = e.target['code-input'].value;
+  const config = { method: 'GET', uri: `/rooms/${code}` };
+  const { ok } = await request(config);
+  if (ok) redirectToGameRoom(code);
+};
+
+const initialize = () => {
+  const makeRoomButton = document.getElementById('create-room');
+  const enterRoomForm = document.getElementById('enter-room');
+
+  makeRoomButton.addEventListener('click', requestMakeRoom);
+  enterRoomForm.addEventListener('submit', requestEnterRoom);
+};
+
+initialize();
