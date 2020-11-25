293c44ea91c4528d84dddc48712cf23914eec2b7
ramram1048 <ramram1048@gmail.com>
2020-11-25 19:24:24 +0900

    feat: InputObj의 value, disabled를 조정할 수 있도록 대응

--- src/frontend/engine/InputObject.js
+++ src/frontend/engine/InputObject.js
@@ -6,8 +6,16 @@ const InputObject = class extends GameObject {
     this.setElement(element);
   }
 
+  setDisabled() {
+    this.instance.setAttribute('disabled', true);
+  }
+
   setPlaceHolder(placeholder) {
     this.placeholder = placeholder;
+  }
+
+  setValue(value) {
+    this.instance.value = value;
   }
 
   addClickHandler(clickHandler) {
