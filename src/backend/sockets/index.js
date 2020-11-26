5bcd4409474bbc0a08e12b38991b91222316b5cd
Front-line-dev <kimminsung@kakao.com>
2020-11-26 10:42:28 +0900

    fix: export 고침

--- src/backend/sockets/index.js
+++ src/backend/sockets/index.js
@@ -1,10 +1,11 @@
 import io from 'socket.io';
 
-// eslint-disable-next-line import/prefer-default-export
-export const socketIO = io();
+const socketIO = io();
 
 socketIO.on('connection', (socket) => {
   console.log('a user connected');
 
   socket.emit('hi', '123');
 });
+
+export default socketIO;
