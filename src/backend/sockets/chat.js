8a5108f68dbd53a43711e3169bd22ce0bf46174d
sbyeol3 <ahnsbyeol3@gmail.com>
2020-11-26 15:50:59 +0900

    Feat : socket 핸들러 함수 구현 (#45)
    - 대기실에서 join 하는 이벤트 핸들러 추가
    - 채팅 전송 시 해당 룸에 있는 소켓에게 메세지 전달하는 핸들러 추가

---
+++ src/backend/sockets/chat.js
@@ -0,0 +1,9 @@
+import { findRoomID } from '@utils/socket';
+
+export const onSendChat = (socket, { message }) => {
+  const roomID = findRoomID(socket);
+  if (!roomID) return;
+
+  socket.in(roomID).emit('send chat', { message });
+  socket.emit('send chat', { message });
+};
