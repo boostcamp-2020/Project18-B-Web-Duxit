0a86761ef5e83693c0f6a91491e3372c2f68f556
ramram1048 <ramram1048@gmail.com>
2020-11-26 15:14:22 +0900

    feat: 우측 채팅창 기능 구현
    - game.js 코드들 함수로 구조화
    - socket 최초 설정 코드를 util 폴더로 이전
    - 채팅 메시지 레이아웃/스타일링

--- src/frontend/game/game.js
+++ src/frontend/game/game.js
@@ -1,13 +1,58 @@
 import './game.scss';
-import ioClient from 'socket.io-client';
-import waitingRoom from '../scenes/waitingRoom';
+import renderWaitingRoom from '../scenes/waitingRoom';
+import socket from '../utils/socket';
 
-const socket = ioClient('http://localhost:3000');
+const initializeLayout = () => {
+  const chatMessageLog = document.getElementById('chat-message-log');
+  const chatForm = document.getElementById('chat-form');
 
-socket.on('hi', (data) => {
-  console.log(data);
-});
+  chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = e.target.message.value;
+    e.target.message.value = '';
 
-const urlParams = new URLSearchParams(window.location.search);
-const roomCode = urlParams.get('room');
-waitingRoom(roomCode);
+    // TODO: initialize에서 분리하기
+    const sendMessageToServer = () => {
+      const messageWrapper = document.createElement('div');
+      const messageBox = document.createElement('div');
+      messageWrapper.classList.add('chat-mine');
+      messageBox.classList.add('chat-message');
+      messageWrapper.appendChild(messageBox);
+      chatMessageLog.appendChild(messageWrapper);
+      messageBox.innerText = message;
+    };
+
+    // sendMessageToServer();
+
+    socket.emit('send chat', { message }, sendMessageToServer);
+  });
+
+  // TODO: initialize에서 분리하기
+  const getMessageFromServer = ({ nickname, message }) => {
+    const messageWrapper = document.createElement('div');
+    const nicknameBox = document.createElement('div');
+    const messageBox = document.createElement('div');
+    messageWrapper.classList.add('chat-other-player');
+    nicknameBox.classList.add('chat-nickname');
+    messageBox.classList.add('chat-message');
+    messageWrapper.appendChild(nicknameBox);
+    messageWrapper.appendChild(messageBox);
+    chatMessageLog.appendChild(messageWrapper);
+    nicknameBox.innerText = nickname;
+    messageBox.innerText = message;
+  };
+
+  socket.on('send chat', getMessageFromServer);
+};
+
+const initialize = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const roomID = urlParams.get('room');
+  socket.emit('join player', { roomID });
+
+  initializeLayout();
+
+  renderWaitingRoom(roomID);
+};
+
+initialize();
