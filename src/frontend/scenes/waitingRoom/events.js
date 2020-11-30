91d42e7509e1c01c4abc9fef2c88cbe03f016c3d
Front-line-dev <kimminsung@kakao.com>
2020-11-30 19:07:32 +0900

    feat: waitingRoom/event 작성

--- src/frontend/scenes/waitingRoom/events.js
+++ src/frontend/scenes/waitingRoom/events.js
@@ -1,3 +1,5 @@
+import socket from '@utils/socket';
+
 export const redirectToLobby = () => {
   window.location.href = '/';
 };
@@ -9,4 +11,15 @@ export const copyGameCode = (e) => {
       navigator.clipboard.writeText(roomID.innerText);
     }
   });
+};
+
+export const toggleReady = ({ target }) => {
+  const { isReady } = JSON.parse(target.dataset.data);
+  // const nextStatus = target.dataset.status === 'not ready';
+  const nextStatus = !isReady;
+  // target.dataset.status = nextStatus ? 'ready' : 'not ready';
+  target.dataset.data = JSON.stringify({ isReady: nextStatus });
+  target.classList.toggle('button-primary');
+  target.classList.toggle('button-primary-clicked');
+  socket.emit('ready player', { isReady: nextStatus });
 };
