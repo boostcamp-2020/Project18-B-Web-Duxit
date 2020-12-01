217d6a58b4d3f398d9c33b3b7d8c084d7d134360
Front-line-dev <kimminsung@kakao.com>
2020-12-01 17:37:46 +0900

    feat: 카드 오브젝트 moveUp 함수
    사용 방법
    Card.move(10,20)
    Card.rotate(30)
    Card.setAnimateMove()
    Card.deleteAnimateMove()

--- src/frontend/engine/CardObject.js
+++ src/frontend/engine/CardObject.js
@@ -3,34 +3,38 @@ import GameObject from './GameObject';
 import ImageObject from './ImageObject';
 
 const getFacingStyle = (isUp) => (isUp ? 'rotateY(0deg)' : 'rotateY(180deg)');
+const deg2rad = (deg) => (deg * Math.PI) / 180;
 
 const CardObject = class extends GameObject {
   constructor({
     imagePath = backImage, // default: 뒷면 이미지
     facingUp = true,
-    hoverable = false,
+    hoverable = false, // <- 안 쓰는게 나을지도?
     origin = ['50%', '50%'],
     ...data
   } = {}) {
     super({ origin, ...data });
     this.facingUp = facingUp;
-    this.setClass('card-wrapper');
+    this.addClass('card-wrapper');
 
     this.inner = new GameObject();
-    this.inner.setClass('card-inner');
+    this.inner.addClass('card-inner');
     this.inner.attachToObject(this);
     this.front = new ImageObject();
-    this.front.setClass('card-common');
+    this.front.addClass('card-common');
     this.front.attachToObject(this.inner);
     const Back = new ImageObject();
     Back.setImagePath(backImage);
     Back.attachToObject(this.inner);
-    Back.setClass('card-common');
-    Back.setClass('card-back');
+    Back.addClass('card-common');
+    Back.addClass('card-back');
 
     this.setImagePath = this.front.setImagePath.bind(this.front);
     this.setImagePath(imagePath);
     this.inner.instance.style.transform = getFacingStyle(facingUp);
+
+    this.hoverMoveUpCallback = null;
+    this.hoverMoveDownCallback = null;
   }
 
   animateFlip(duration = 0.5, toUp = !this.facingUp) {
@@ -46,6 +50,30 @@ const CardObject = class extends GameObject {
     }
     this.facingUp = !this.facingUp;
     this.inner.instance.style.transform = next;
+  }
+
+  setAnimateMove() {
+    const [fixedX, fixedY] = this.position;
+    const MOVE_PERCENT = 10;
+
+    this.hoverMoveUpCallback = () => {
+      const movedX = fixedX + Math.sin(deg2rad(this.angle)) * MOVE_PERCENT;
+      const movedY = fixedY - Math.cos(deg2rad(this.angle)) * MOVE_PERCENT;
+
+      this.move(movedX, movedY);
+    };
+
+    this.hoverMoveDownCallback = () => {
+      this.move(fixedX, fixedY);
+    };
+
+    this.instance.addEventListener('mouseover', this.hoverMoveUpCallback);
+    this.instance.addEventListener('mouseleave', this.hoverMoveDownCallback);
+  }
+
+  deleteAnimateMove() {
+    this.instance.removeEventListener('mouseover', this.hoverMoveUpCallback);
+    this.instance.removeEventListener('mouseleave', this.hoverMoveDownCallback);
   }
 };
 
