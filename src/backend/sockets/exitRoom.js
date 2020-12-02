d1c7e1541475cd37c20fc02191b8e1235157b0d3
jinhyukoo <faill456@naver.com>
2020-12-02 16:11:47 +0900

    feat: 백엔드 socket 설정
    - 유저가 disconnect 됐을 경우 exit player를 다른 유저들에게 뿌려줌
    - 에러 방지를 위해 removeUser가 마지막에 되게 설정

--- src/backend/sockets/exitRoom.js
+++ src/backend/sockets/exitRoom.js
@@ -1,9 +1,13 @@
 function exitRoom() {
   const socket = this;
-  socket.game?.removeUser({ socketID: socket.id });
   if (process.env.NODE_ENV === 'development') {
     console.log(`user disconnected ${socket.id}`);
   }
+  const { roomID } = socket.game;
+  const passedData = { socketID: socket.id };
+  socket.in(roomID).emit('exit player', passedData);
+
+  socket.game?.removeUser({ socketID: socket.id });
 }
 
 export default function onWaitingRoom(socket) {
