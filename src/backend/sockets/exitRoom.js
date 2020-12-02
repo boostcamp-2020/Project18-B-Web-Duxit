206c5cc31fa173dd3b93d1f669009ecd67e66a4c
ramram1048 <ramram1048@gmail.com>
2020-12-02 18:03:58 +0900

    fix: 마지막으로 사람이 나가면 터지는 문제

--- src/backend/sockets/exitRoom.js
+++ src/backend/sockets/exitRoom.js
@@ -3,6 +3,7 @@ function exitRoom() {
   if (process.env.NODE_ENV === 'development') {
     console.log(`user disconnected ${socket.id}`);
   }
+  if (!socket.game) return;
   const { roomID } = socket.game;
   const passedData = { socketID: socket.id };
   socket.in(roomID).emit('exit player', passedData);
