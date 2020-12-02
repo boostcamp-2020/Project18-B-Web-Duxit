3ec7b9f6f11d4b0a72d5b54701290c2b48d69fda
jinhyukoo <faill456@naver.com>
2020-12-03 00:42:41 +0900

    feat: 랜덤 색상 함수 구현 및 적용

--- src/backend/utils/generateRandom.js
+++ src/backend/utils/generateRandom.js
@@ -4,6 +4,14 @@ import nickname from './nickname.json';
 const generateRandomString = () =>
   Math.random().toString(36).substr(2, 5).toUpperCase();
 
+const generateRandomColor = () => {
+  const r = Math.round(Math.random() * 255).toString(16);
+  const g = Math.round(Math.random() * 255).toString(16);
+  const b = Math.round(Math.random() * 255).toString(16);
+  const color = `#${r}${g}${b}`;
+  return color;
+};
+
 const randomFunctions = {
   nickname: () => {
     const { adjective: adj, noun } = nickname;
@@ -11,7 +19,9 @@ const randomFunctions = {
     const randomNoun = noun[Math.floor(Math.random() * noun.length)];
     return `${randomAdj} ${randomNoun}`;
   },
-  color: () => '#222222',
+  color: () => {
+    return generateRandomColor();
+  },
   roomID: () => {
     const randomString = generateRandomString();
     if (GameList.hasGame(randomString)) return randomFunctions.roomID();
