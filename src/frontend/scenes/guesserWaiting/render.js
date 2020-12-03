67a50bbf6cd27ff409120b7827e16cf895efac4b
jinhyukoo <faill456@naver.com>
2020-12-03 15:06:51 +0900

    feat: GuesserWaiting 화면 UI
    - 카드 위치 조정
    - 카드, 오리, 안내메시지 등장 시간 조정

--- src/frontend/scenes/guesserWaiting/render.js
+++ src/frontend/scenes/guesserWaiting/render.js
@@ -8,6 +8,10 @@ import TEXT from '@utils/text';
 import TIME from '@utils/time';
 import { DUCK_TYPE } from '@utils/type';
 
+const xPoint = [37.5, 42.5, 47.5, 52.5, 57.5, 62.5];
+const yPoint = [20, 17, 15, 15, 17, 20];
+const angle = [-25, -15, -5, 5, 15, 25];
+let count = 0;
 const createCards = () => {
   const emptyObject = new GameObject();
   emptyObject.createElement();
@@ -15,7 +19,10 @@ const createCards = () => {
   const cards = Array.from({ length: 6 }, () => {
     const card = new CardObject();
     card.addClass('teller-duck-card');
-    card.move(10, 10, 0);
+    card.move(50, 0, 0);
+    card.rotate(angle[count], 0);
+    card.move(xPoint[count], yPoint[count], 1);
+    count += 1;
     emptyObject.appendChild(card);
     return card;
   });
@@ -29,18 +36,20 @@ const renderGuesserWaiting = () => {
   NotifyingTellerText.addClass('other');
   NotifyingTellerText.attachToRoot();
   NotifyingTellerText.setContent(tellerText);
+  NotifyingTellerText.move(50, 100, 0);
+  NotifyingTellerText.move(50, 70, 1);
 
   const ProgressBar = new ProgressBarObject();
   ProgressBar.createElement();
   ProgressBar.attachToRoot();
   ProgressBar.setTime(TIME.SELECT_CARD);
   ProgressBar.start();
-
   const TellerDuck = new DuckObject({ type: DUCK_TYPE.TELLER });
   // TODO : setColor 텔러에 맞는 색깔로 하면 될까?
   TellerDuck.createElement();
   TellerDuck.attachToRoot();
-
+  TellerDuck.move(50, 0, 0);
+  TellerDuck.move(50, 10, 1);
   const { CardsWrapper, cards } = createCards();
   CardsWrapper.attachToRoot();
 
