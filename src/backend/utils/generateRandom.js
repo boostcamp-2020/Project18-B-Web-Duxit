f9a6412d11d0730339899ff6ae42827a1373e82a
jinhyukoo <faill456@naver.com>
2020-12-03 10:40:32 +0900

    refactor: 피드백 반영
    - 함수 구조 변경

--- src/backend/utils/generateRandom.js
+++ src/backend/utils/generateRandom.js
@@ -4,14 +4,6 @@ import nickname from './nickname.json';
 const generateRandomString = () =>
   Math.random().toString(36).substr(2, 5).toUpperCase();
 
-const generateRandomColor = () => {
-  const r = Math.round(Math.random() * 255).toString(16);
-  const g = Math.round(Math.random() * 255).toString(16);
-  const b = Math.round(Math.random() * 255).toString(16);
-  const color = `#${r}${g}${b}`.padEnd(7, '0');
-  return color;
-};
-
 const randomFunctions = {
   nickname: () => {
     const { adjective: adj, noun } = nickname;
@@ -20,7 +12,11 @@ const randomFunctions = {
     return `${randomAdj} ${randomNoun}`;
   },
   color: () => {
-    return generateRandomColor();
+    const r = Math.round(Math.random() * 255).toString(16);
+    const g = Math.round(Math.random() * 255).toString(16);
+    const b = Math.round(Math.random() * 255).toString(16);
+    const color = `#${r}${g}${b}`.padEnd(7, '0');
+    return color;
   },
   roomID: () => {
     const randomString = generateRandomString();
