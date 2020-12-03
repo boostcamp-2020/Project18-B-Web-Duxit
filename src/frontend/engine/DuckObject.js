da60a1b806e65599a0168b45c7c4d14f6e779449
sbyeol3 <ahnsbyeol3@gmail.com>
2020-12-03 16:19:54 +0900

    Feat : duck 모자 SVG 추가 (#107)
    - 모자 SVG 추가
    - width params로 넘겨주기
    - 오리 사이즈 줄임

--- src/frontend/engine/DuckObject.js
+++ src/frontend/engine/DuckObject.js
@@ -1,7 +1,6 @@
 import { $create } from '@utils/dom';
 import { DUCK_TYPE } from '@utils/type';
-
-import Duck from '@utils/duck';
+import Duck, { DuckHat } from '@utils/duck';
 import ImageObject from './ImageObject';
 
 const DuckObject = class extends ImageObject {
@@ -56,10 +55,10 @@ const DuckObject = class extends ImageObject {
 
   getComponentForLeft() {
     const { nickname, color, score, hat } = this;
-    const option = { color, width: 65 };
+    const option = { color, width: 50 };
     return `
         <div class="duck-image">
-          ${hat ? '<span class="duck-hat">hat</span>' : ''}
+          ${hat ? DuckHat({ width: 40 }) : ''}
           ${Duck(option)}
           <span class="duck-score">${score}</span>
         </div>
