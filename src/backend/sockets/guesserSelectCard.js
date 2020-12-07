c7f27d6a6bafd8b025655b347d46b8f820f355ef
sbyeol3 <ahnsbyeol3@gmail.com>
2020-12-07 22:30:14 +0900

    Feat : 서버 socket 함수 수정
    - teller select : 게임 상태 변경
    - guesser select : 모든 플레이어에게 cardID 보냄?

--- src/backend/sockets/guesserSelectCard.js
+++ src/backend/sockets/guesserSelectCard.js
@@ -4,15 +4,12 @@ function onSendGuesserDecision({ cardID }) {
   const socket = this;
   const { game, user } = socket;
 
-  // Error handling
   if (!game || !user) return;
   if (game.status.state !== GAME_STATE.GUESSER) return;
 
-  // Save cardID into Game.user
   user.submitCard(cardID);
-
-  // Emit socket
   socket.emit('guesser select card', { cardID });
+  socket.in(game.roomID).emit('guesser select card', { cardID });
 }
 
 export default function onGuesserSelectCard(socket) {
