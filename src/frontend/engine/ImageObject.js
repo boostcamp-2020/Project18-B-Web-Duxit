0818492e71ecd27fdffdc0b88cc650d8a8198fda
ramram1048 <ramram1048@gmail.com>
2020-12-08 17:19:58 +0900

    feat: ImageObject가 constructor에서 imagePath를 설정하게 함

--- src/frontend/engine/ImageObject.js
+++ src/frontend/engine/ImageObject.js
@@ -1,6 +1,11 @@
 import GameObject from './GameObject';
 
 const ImageObject = class extends GameObject {
+  constructor({ imagePath, ...rest }) {
+    super(rest);
+    this.setImagePath(imagePath);
+  }
+
   createElement() {
     const element = document.createElement('img');
     this.setElement(element);
