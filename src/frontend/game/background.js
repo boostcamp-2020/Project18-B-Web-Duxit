4f75290233845c5c980f010766154449bc98679b
ramram1048 <ramram1048@gmail.com>
2020-12-08 17:47:52 +0900

    feat: 돌땡이 설치
    - stonePosition json을 이용해서 돌땡이 생성
    - 돌땡이에 점수번호 text로 표시
    - root가 100% scale 상태일땐 돌땡이가 안보이도록 z-index 지정

--- src/frontend/game/background.js
+++ src/frontend/game/background.js
@@ -1,3 +1,28 @@
-const initializeBackground = () => {};
+import { $id } from '@utils/dom';
+import ImageObject from '@engine/ImageObject';
+import TextObject from '@engine/TextObject';
+import stonePath from '@resources/stone.png';
+import stonePosition from '@type/stonePosition.json';
+import GameObject from '../engine/GameObject';
+
+const initializeBackground = () => {
+  const background = $id('background');
+  stonePosition.forEach((position, index) => {
+    const wrapper = new GameObject({
+      classes: ['stone'],
+      position,
+      origin: [50, 30],
+    });
+    const stone = new ImageObject({
+      imagePath: stonePath,
+      parent: wrapper,
+    });
+    const number = new TextObject({
+      parent: wrapper,
+      classes: ['stone-text'],
+    }).setContent(index + 1);
+    background.appendChild(wrapper.instance);
+  });
+};
 
 export default initializeBackground;
