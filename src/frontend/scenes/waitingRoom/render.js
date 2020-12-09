f708f6a5a7a525ef1feb593d9c5d06f0388b696a
ramram1048 <ramram1048@gmail.com>
2020-12-09 17:39:05 +0900

    feat: 색상 변경 레이아웃/스타일링 #162
    - waitingRoom/render가 상당히 지저분해져서 한번 정리 필요할것같음

--- src/frontend/scenes/waitingRoom/render.js
+++ src/frontend/scenes/waitingRoom/render.js
@@ -5,6 +5,7 @@ import TextObject from '@engine/TextObject';
 import InputObject from '@engine/InputObject';
 import SvgObject from '@engine/SvgObject';
 import Svg from '@type/svg';
+import defaultColors from '@type/defaultColors.json';
 import {
   copyGameCode,
   redirectToLobby,
@@ -26,6 +27,67 @@ const renderWaitingRoom = (roomID = '') => {
   InputWrapper.addClass('waiting-input-wrapper');
   InputWrapper.attachToObject(Header);
 
+  const ColorButtonWrapper = new GameObject({
+    classes: ['color-button-wrapper'],
+    parent: InputWrapper,
+  });
+
+  const ColorButton = new ButtonObject({
+    classes: ['refresh-button'],
+    parent: ColorButtonWrapper,
+  });
+
+  const BrushIcon = new SvgObject();
+  BrushIcon.setSvg(Svg.brush);
+  BrushIcon.attachToObject(ColorButton);
+
+  const ColorPickerWrapper = new GameObject({
+    classes: ['color-picker-wrapper'],
+    parent: ColorButtonWrapper,
+  });
+
+  const ColorPickerTops = new GameObject({
+    parent: ColorPickerWrapper,
+  });
+
+  const RandomColorButton = new ButtonObject({
+    classes: ['refresh-button', 'refresh-button-small'],
+    parent: ColorPickerTops,
+  });
+
+  const RandomIcon = new SvgObject();
+  RandomIcon.setSvg(Svg.refresh);
+  RandomIcon.attachToObject(RandomColorButton);
+
+  const ColorInput = new InputObject({
+    classes: ['waiting-color-input'],
+    parent: ColorPickerTops,
+  });
+  ColorInput.setAttributes({
+    maxlength: 8,
+    size: 7,
+    autocomplete: 'off',
+    placeholder: '#',
+    title: '16진 색상 코드를 직접 입력해보세요.',
+    value: '#',
+  });
+
+  const ColorSampleGrid = new GameObject({
+    classes: ['color-sample-grid'],
+    parent: ColorPickerWrapper,
+  });
+
+  defaultColors.forEach((color) => {
+    const sampleButton = new ButtonObject({
+      classes: ['color-sample-button'],
+      parent: ColorSampleGrid,
+    });
+    sampleButton.setAttributes({
+      title: color,
+    });
+    sampleButton.instance.style.backgroundColor = color;
+  });
+
   const NicknameInput = new InputObject();
   NicknameInput.addClass('waiting-nickname-input');
   NicknameInput.setAttributes({
@@ -35,14 +97,14 @@ const renderWaitingRoom = (roomID = '') => {
   });
   NicknameInput.attachToObject(InputWrapper);
 
-  const RefreshButton = new ButtonObject();
-  RefreshButton.addClass('refresh-button');
-  RefreshButton.attachToObject(InputWrapper);
-  RefreshButton.addClickHandler(() => changeNickname(NicknameInput));
+  const NicknameSubmitButton = new ButtonObject();
+  NicknameSubmitButton.addClass('refresh-button');
+  NicknameSubmitButton.attachToObject(InputWrapper);
+  NicknameSubmitButton.addClickHandler(() => changeNickname(NicknameInput));
 
   const CheckIcon = new SvgObject();
   CheckIcon.setSvg(Svg.check);
-  CheckIcon.attachToObject(RefreshButton);
+  CheckIcon.attachToObject(NicknameSubmitButton);
 
   const ActionWrapper = new GameObject();
   ActionWrapper.addClass('waiting-action-wrapper');
--- src/frontend/type/svg.js
+++ src/frontend/type/svg.js
@@ -1,5 +1,6 @@
 export default {
   check: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="48px" height="48px"><path d="M0 0h24v24H0z" fill="none"/><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/></svg>`,
+  brush: `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M7 14c-1.66 0-3 1.34-3 3 0 1.31-1.16 2-2 2 .92 1.22 2.49 2 4 2 2.21 0 4-1.79 4-4 0-1.66-1.34-3-3-3zm13.71-9.37l-1.34-1.34c-.39-.39-1.02-.39-1.41 0L9 12.25 11.75 15l8.96-8.96c.39-.39.39-1.02 0-1.41z"/></svg>`,
   refresh: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="48px" height="48px"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 6v3l4-4-4-4v3c-4.42 0-8 3.58-8 8 0 1.57.46 3.03 1.24 4.26L6.7 14.8c-.45-.83-.7-1.79-.7-2.8 0-3.31 2.69-6 6-6zm6.76 1.74L17.3 9.2c.44.84.7 1.79.7 2.8 0 3.31-2.69 6-6 6v-3l-4 4 4 4v-3c4.42 0 8-3.58 8-8 0-1.57-.46-3.03-1.24-4.26z"/></svg>`,
   copy: `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/></svg>`,
 };
