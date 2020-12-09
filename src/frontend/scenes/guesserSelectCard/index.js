c78f7223609e86e6914bddf7e3744408a8aafb22
sbyeol3 <ahnsbyeol3@gmail.com>
2020-12-09 16:48:30 +0900

    Feat : GuesserSelectCard에서 endtime 받기
    - endtime받아서 render에 넘겨줌

--- src/frontend/scenes/guesserSelectCard/index.js
+++ src/frontend/scenes/guesserSelectCard/index.js
@@ -2,8 +2,13 @@ import renderGuesserSelect from './render';
 import setupGuesserSelectCard from './socket';
 
 const GuesserSelectCard = class {
+  constructor({ endTime }) {
+    this.endTime = endTime;
+  }
+
   render() {
-    const { arrayToBeRemoved, ProgressBar } = renderGuesserSelect();
+    const { endTime } = this;
+    const { arrayToBeRemoved, ProgressBar } = renderGuesserSelect({ endTime });
     this.arrayToBeRemoved = arrayToBeRemoved;
     setupGuesserSelectCard({ ProgressBar });
   }
