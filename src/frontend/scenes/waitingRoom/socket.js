c4428737911a2da1560e551b9b02c2b82bbd6ca0
sbyeol3 <ahnsbyeol3@gmail.com>
2020-12-10 15:31:15 +0900

    Refactort : game의 소켓 이벤트를 waitingRoom으로 이동
    - 대기실에서만 발생하는 것들은 대기실 소켓파일로 이동

--- src/frontend/scenes/waitingRoom/socket.js
+++ src/frontend/scenes/waitingRoom/socket.js
@@ -1,6 +1,16 @@
 import socket from '@utils/socket';
+import PlayerManager from '@utils/PlayerManager';
+import SceneManager from '@utils/SceneManager';
+import CardManager from '@utils/CardManager';
+import TellerSelectCard from '@scenes/tellerSelectCard';
+import GuesserWaiting from '@scenes/guesserWaiting';
 
 const setupWaitingRoomSocket = ({ AllReadyText }) => {
+  const onEnterRoom = ({ nickname, players }) => {
+    PlayerManager.initialize(players);
+    PlayerManager.updateCurrentPlayer({ nickname });
+  };
+
   const onAllReady = () => {
     AllReadyText.removeClass('hide');
     AllReadyText.instance.animate(
@@ -20,8 +30,36 @@ const setupWaitingRoomSocket = ({ AllReadyText }) => {
     // TODO: 게임 시작 취소됐다는 메시지 띄워줘야됨~~
   };
 
+  const onUpdatePlayer = ({ socketID, nickname, color }) => {
+    PlayerManager.set({ socketID, nickname, color });
+  };
+
+  const onExitPlayer = ({ socketID }) => {
+    PlayerManager.delete(socketID);
+  };
+
+  const onReadyPlayer = ({ playerID, isReady }) => {
+    const player = PlayerManager.get(playerID);
+    if (player) player.setReady(isReady);
+  };
+
+  const onGetRoundData = ({ tellerID, cards, endTime }) => {
+    PlayerManager.setTellerID(tellerID);
+    CardManager.initailizeMyCards(cards);
+    const { isTeller } = PlayerManager.getCurrentPlayer();
+    const nextScene = isTeller
+      ? new TellerSelectCard({ cards, endTime })
+      : new GuesserWaiting({ endTime });
+    SceneManager.renderNextScene(nextScene);
+  };
+
+  socket.on('enter room', onEnterRoom);
+  socket.on('update player', onUpdatePlayer);
+  socket.on('exit player', onExitPlayer);
+  socket.on('ready player', onReadyPlayer);
   socket.on('all ready', onAllReady);
   socket.on('game start aborted', onGameStartAborted);
+  socket.on('get round data', onGetRoundData);
 };
 
 export default setupWaitingRoomSocket;
