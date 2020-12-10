10e79d108eb37d55e06d9149c8a6fbb8c09f0684
sbyeol3 <ahnsbyeol3@gmail.com>
2020-12-10 17:27:09 +0900

    Feat : 각 씬별로 오리 컨트롤 (#177)
    - 이슈번호 177 구현

--- src/frontend/scenes/guesserSelectCard/socket.js
+++ src/frontend/scenes/guesserSelectCard/socket.js
@@ -2,15 +2,19 @@ import socket from '@utils/socket';
 import CardManager from '@utils/CardManager';
 import SceneManager from '@utils/SceneManager';
 import PlayerWaiting from '../playerWaiting';
+import PlayerManager from '../../utils/PlayerManager';
 
 const setupGuesserSelectCard = ({ ProgressBar, scene }) => {
   const onGuesserSelectCard = ({ cardID }) => {
     CardManager.addSubmittedCardCount();
     CardManager.selectCard(cardID);
     SceneManager.renderNextScene(new PlayerWaiting({ ProgressBar }));
+
+    const myDuck = PlayerManager.getCurrentPlayer().duck;
+    myDuck.setVisibility(true, true);
   };
 
-  const onOtherGuesserSelectCard = ({ playerID }) => {
+  const onOtherGuesserSelectCard = () => {
     if (SceneManager.currentScene === scene) {
       CardManager.addSubmittedCardCount();
     }
