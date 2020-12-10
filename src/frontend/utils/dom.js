eee6de3000c0cd52c7ecbcbc327ef05b2ede0cc7
ramram1048 <ramram1048@gmail.com>
2020-12-10 18:19:52 +0900

    feat: @utils/dom에서 #root, #background를 바로 찾게 대응

--- src/frontend/utils/dom.js
+++ src/frontend/utils/dom.js
@@ -1,3 +1,6 @@
 export const $qs = document.querySelector.bind(document);
 export const $id = document.getElementById.bind(document);
 export const $create = (tag) => document.createElement(tag);
+
+export const ROOT = $id('root');
+export const BACKGROUND = $id('background');
