63c150fd944738f11bc4e568389995983acffe5f
ramram1048 <ramram1048@gmail.com>
2020-12-10 18:04:00 +0900

    feat: Scoreboard 각 플레이어의 획득점수 표시
    - 사용되지 않은 변수들이 const로 남겨져있는데 지워야될지 말아야될지...

--- src/frontend/scenes/scoreboard/render.js
+++ src/frontend/scenes/scoreboard/render.js
@@ -1,6 +1,50 @@
 import GameObject from '@engine/GameObject';
+import DuckObject from '@engine/DuckObject';
+import TextObject from '@engine/TextObject';
 import './style.scss';
 import template from './template.html';
+
+const renderRow = (TableBody) => ({
+  nickname,
+  color,
+  isTeller,
+  isCurrentPlayer,
+  score: currentScore,
+  correctScore,
+  bonusScore,
+  totalScore,
+} = {}) => {
+  const TableRow = new GameObject({
+    classes: ['scoreboard-table-row'],
+    parent: TableBody,
+  });
+  const PlayerInfoWrapper = new GameObject({
+    classes: ['scoreboard-table-player'],
+    parent: TableRow,
+  });
+  const DuckIcon = new DuckObject({
+    color,
+    width: 60,
+    parent: PlayerInfoWrapper,
+  });
+  DuckIcon.setHat(isTeller);
+  const NicknameObject = new TextObject({
+    parent: PlayerInfoWrapper,
+  }).setContent(nickname);
+
+  const CorrectScoreObject = new TextObject({
+    classes: ['scoreboard-table-score'],
+    parent: TableRow,
+  }).setContent(`+${correctScore}`);
+  const BonusScoreObject = new TextObject({
+    classes: ['scoreboard-table-score'],
+    parent: TableRow,
+  }).setContent(`+${bonusScore}`);
+  const TotalScoreObject = new TextObject({
+    classes: ['scoreboard-table-score'],
+    parent: TableRow,
+  }).setContent(`${totalScore}`);
+};
 
 const renderScoreboardLayout = ({ round, players, scoreData } = {}) => {
   const templateWrapper = document.createElement('div');
@@ -18,6 +62,14 @@ const renderScoreboardLayout = ({ round, players, scoreData } = {}) => {
   TableBody.instance = Background.instance.querySelector(
     '#scoreboard-table-body',
   );
+
+  const playersWithScore = players.map((player) => ({
+    ...player,
+    ...scoreData.find((data) => data.socketID === player.socketID),
+  }));
+  const renderRowInTableBody = renderRow(TableBody);
+
+  playersWithScore.forEach(renderRowInTableBody);
 
   const arrayToBeRemoved = [Background];
   return {
