ffeb09d8e8ad55f2d9b85627aeb0aca98704db0f
Front-line-dev <kimminsung@kakao.com>
2020-12-13 22:49:50 +0900

    refactor: socket/discussion 잘못된 로직 삭제
    아예 새로 짜야함

--- src/backend/sockets/discussion.js
+++ src/backend/sockets/discussion.js
@@ -1,45 +1,23 @@
 import { TIME } from '@utils/number';
 
-const isPossibleSkipDiscussion = ({ users }) => {
-  const isAllSkip = [...users].every(([, user]) => user.isSkip);
-  return isAllSkip;
-};
-
 function onSkipPlayer() {
   const socket = this;
-  const { game } = socket;
-  const { users, roomID } = game;
-  users.get(socket.id).setSkip();
+  const { user, game } = socket;
 
-  const validationToSkipDiscussion = isPossibleSkipDiscussion({ users });
-  if (validationToSkipDiscussion) {
-    socket.in(roomID).emit('all skip');
+  user.setSkip();
+
+  // 모든 유저가 스킵을 눌렀을 경우
+  if (game.getUsers().every((u) => u.isSkip)) {
+    socket.in(game.roomID).emit('all skip');
     socket.emit('all skip');
     setTimeout(() => {
-      socket.in(roomID).emit('end discussion');
+      socket.in(game.roomID).emit('end discussion');
       socket.emit('end discussion');
     }, TIME.SKIP_DISCUSSION);
   }
 }
 
-function onStartDiscussion() {
-  const socket = this;
-  const { game } = socket;
-  const { roomID } = game;
-  if (!game.status.firstDiscussionUser) {
-    game.setEndTime(TIME.WAIT_DISCUSSION);
-    game.toggleFirstDiscussionUser();
-  }
-  const { endTime } = game;
-  socket.in(roomID).emit('start discussion', { endTime });
-  socket.emit('start discussion', { endTime });
-  const currentTime = new Date().getTime();
-  const targetTime = new Date(endTime - currentTime).getTime();
-  setTimeout(() => {
-    socket.in(roomID).emit('end discussion');
-    socket.emit('end discussion');
-  }, targetTime);
-}
+function onStartDiscussion() {}
 
 export default function onDiscussion(socket) {
   socket.on('skip player', onSkipPlayer);
