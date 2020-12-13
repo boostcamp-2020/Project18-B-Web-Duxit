9381fabdee6a29697483e6edbbf2d742cc3a914f
Front-line-dev <kimminsung@kakao.com>
2020-12-14 01:21:00 +0900

    feat: 프론트엔드 강제 종료 코드

--- src/frontend/utils/socket.js
+++ src/frontend/utils/socket.js
@@ -2,6 +2,12 @@ import ioClient from 'socket.io-client';
 
 const initializeSocket = () => {
   const socket = ioClient(process.env.API_BASE_URL);
+
+  socket.on('game terminated', () => {
+    alert('게임 도중 플레이어가 나가서 게임이 종료됩니다');
+    window.location.href = '/';
+  });
+
   return socket;
 };
 
