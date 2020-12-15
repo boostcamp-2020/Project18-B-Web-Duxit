5ff35c794180441e5725259a1a000130f4aa866a
Front-line-dev <kimminsung@kakao.com>
2020-12-15 13:20:47 +0900

    fix: 승리 스코어 조건 고침

--- src/backend/game/GameMethods/scoreBoardScene.js
+++ src/backend/game/GameMethods/scoreBoardScene.js
@@ -7,7 +7,8 @@ const isGameOver = (game) => {
   if (game.status.unusedCards.length < game.getUsers().length) return true;
 
   // 플레이어 최소 한명이 30점을 넘겼을 경우
-  if (game.getUsers().some((user) => user.score > SCORE.WIN_SCORE)) return true;
+  if (game.getUsers().some((user) => user.score >= SCORE.WIN_SCORE))
+    return true;
 
   return false;
 };
@@ -15,6 +16,7 @@ const isGameOver = (game) => {
 function startScoreBoardScene() {
   this.setState(GAME_STATE.SCORE);
   this.emitRoundScore();
+
   setTimeout(() => {
     this.endScoreBoardScene();
   }, TIME.WAIT_SCORE_BOARD);
