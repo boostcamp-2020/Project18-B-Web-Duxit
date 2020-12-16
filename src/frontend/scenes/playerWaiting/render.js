8e9e77ea36bb861929d62c622ae55e0e73ade1d0
jinhyukoo <faill456@naver.com>
2020-12-16 15:33:23 +0900

    feat: 화면 전환시 modal창 닫히게 하기
    - select에서 playerWaiting 화면으로 넘어가면 모달창 자동으로 닫히게 함

--- src/frontend/scenes/playerWaiting/render.js
+++ src/frontend/scenes/playerWaiting/render.js
@@ -3,11 +3,17 @@ import TextObject from '@engine/TextObject';
 import PlayerManager from '@utils/PlayerManager';
 import SceneManager from '@utils/SceneManager';
 import TEXT from '@utils/text';
+import { $qs } from '@utils/dom';
 
 const renderPlayerWaiting = ({ endTime }) => {
   const isTeller = PlayerManager.isTeller();
   const tellerDuck = PlayerManager.getTeller().duck;
   tellerDuck.setVisibility(true, isTeller);
+
+  const modal = $qs('.modal');
+  if (modal) {
+    modal.remove();
+  }
 
   if (isTeller) {
     const { ProgressBar } = SceneManager.sharedComponents;
