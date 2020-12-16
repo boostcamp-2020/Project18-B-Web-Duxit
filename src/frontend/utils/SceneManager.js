a06e70de51d35410f5ff6b1d8dd47172ebf008a2
sbyeol3 <ahnsbyeol3@gmail.com>
2020-12-16 15:41:15 +0900

    Feat : 씬 매니저에 이전에 제출한 플레이어 저장 (#252)
    - 씬 매니저에 관리
    - 커서덕들 숨기는 거 조건문 추가

--- src/frontend/utils/SceneManager.js
+++ src/frontend/utils/SceneManager.js
@@ -11,6 +11,7 @@ const root = $id('root');
 const SceneManager = {
   currentScene: null,
   sharedComponents: [],
+  beforeSubmittingPlayers: [],
 
   initializeComponents() {
     const AllReadyText = new TextObject();
@@ -38,13 +39,14 @@ const SceneManager = {
   renderNextScene(scene, ...args) {
     const { ProgressBar } = this.sharedComponents;
     let wrapupInterval = TIME.NONE_INTERVAL;
-    this.hideAllDucks();
 
     if (this.currentScene) {
       if (!this.currentScene.passingTimerClear) ProgressBar.clear();
       this.currentScene.wrapup();
       wrapupInterval = this.currentScene.wrapupInterval || TIME.NONE_INTERVAL;
     }
+    if (!scene.bMaintainDucks) this.hideAllDucks();
+
     setTimeout(() => {
       scene.render(root, args);
       this.currentScene = scene;
@@ -60,6 +62,14 @@ const SceneManager = {
     players.forEach((player) =>
       player.duck.setVisibility(false, player.isCurrentPlayer),
     );
+  },
+
+  addBeforeSubmittingPlayers(playerID) {
+    this.beforeSubmittingPlayers = [...this.beforeSubmittingPlayers, playerID];
+  },
+
+  initializeSubmiitingPlayers() {
+    this.beforeSubmittingPlayers = [];
   },
 };
 
